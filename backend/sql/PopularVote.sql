43dd7eb5229c20c634a5cb7ec89161970908b7f6
ahoeffner <alex@hoeffner.net>
2023-12-19 23:17:14 +0100

    Update PopularVote.sql

--- backend/sql/PopularVote.sql
+++ backend/sql/PopularVote.sql
@@ -1,6 +1,4 @@
-/*
- * Run as proviliged user
- */
+/* Run as proviliged user */
 
 create user pvuser; 
 create user pvowner; 
@@ -16,17 +14,17 @@ alter user pvpublic with password 'pvpublic';
 
 create database PopularVote with owner pvowner;
 
-/*
- * Change to pvowner
- */
 
+
+/* Change to pvowner */
 
 create schema data;
 
 
+drop table data.users ;
 create table data.users
 (
-	id bigint generated always as identity,
+	id bigint generated always as identity primary key,
 	name varchar(40) not null,
 	email varchar(40) not null,
 	password varchar(40) not null
@@ -41,7 +39,7 @@ grant select on data.users to pvuser;
 
 
 grant usage on schema data to pvpublic;
-grant select on data.users to pvpublic;
+grant insert, select on data.users to pvpublic;
 
 
 insert into data.users (name,email,password) values ('Alex Høffner','alex@hoeffner.net','XYZ')
